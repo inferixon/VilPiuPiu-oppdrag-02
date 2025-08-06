2701dbde749fdc58e63679e3d9624a0b3a681d4c
inferixon <alekzmay@gmail.com>
2025-08-06 15:57:13 +0200

    Refactor SpriteKind definitions by removing unused constants and clean up main game logic

--- main_clean.ts
+++ main_clean.ts
@@ -2,6 +2,22 @@ namespace SpriteKind {
     export const Weapon = SpriteKind.create()
     export const Asteroid = SpriteKind.create()
 }
+
+let laser: Sprite = null
+let starship: Sprite = null
+let asteroid: Sprite = null
+let stjerne: Sprite = null
+let ast = 0
+
+scene.setBackgroundImage(assets.image`main_bg`)
+info.setScore(0)
+info.setLife(3)
+
+starship = sprites.create(assets.image`starship`, SpriteKind.Player)
+starship.setPosition(73, 105)
+starship.setStayInScreen(true)
+controller.moveSprite(starship, 100, 30)
+
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     laser = sprites.create(assets.image`lasershot-2x`, SpriteKind.Weapon)
     laser.setPosition(starship.x - 0, starship.y - 8)
@@ -9,14 +25,13 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     laser.setFlag(SpriteFlag.AutoDestroy, true)
     music.play(music.createSoundEffect(WaveShape.Triangle, 1288, 137, 255, 0, 320, SoundExpressionEffect.Tremolo, InterpolationCurve.Curve), music.PlaybackMode.InBackground)
 })
+
 sprites.onOverlap(SpriteKind.Weapon, SpriteKind.Asteroid, function (sprite, otherSprite) {
     sprites.destroy(otherSprite, effects.fire, 300)
     info.changeScoreBy(1)
     music.play(music.createSoundEffect(WaveShape.Noise, 1, 1534, 255, 0, 150, SoundExpressionEffect.Vibrato, InterpolationCurve.Linear), music.PlaybackMode.InBackground)
 })
-info.onScore(100, function () {
-    game.gameOver(true)
-})
+
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Asteroid, function (sprite, otherSprite) {
     music.play(music.createSoundEffect(WaveShape.Noise, 1, 147, 99, 0, 404, SoundExpressionEffect.Vibrato, InterpolationCurve.Linear), music.PlaybackMode.InBackground)
     sprites.destroy(otherSprite, effects.fire, 300)
@@ -24,21 +39,15 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Asteroid, function (sprite, othe
     scene.cameraShake(10, 200)
     pause(200)
 })
+
+info.onScore(100, function () {
+    game.gameOver(true)
+})
+
 info.onLifeZero(function () {
     game.gameOver(false)
 })
-let stjerne: Sprite = null
-let asteroid: Sprite = null
-let ast = 0
-let laser: Sprite = null
-let starship: Sprite = null
-scene.setBackgroundImage(assets.image`main_bg`)
-info.setScore(0)
-info.setLife(3)
-starship = sprites.create(assets.image`starship`, SpriteKind.Player)
-starship.setPosition(73, 105)
-starship.setStayInScreen(true)
-controller.moveSprite(starship, 100, 30)
+
 game.onUpdateInterval(500, function () {
     if (Math.percentChance(34)) {
         ast = randint(1, 3)
@@ -73,6 +82,7 @@ game.onUpdateInterval(500, function () {
         asteroid.setFlag(SpriteFlag.AutoDestroy, true)
     }
 })
+
 game.onUpdateInterval(100, function () {
     if (Math.percentChance(34)) {
         stjerne = sprites.createProjectileFromSide(assets.image`stjerne`, 0, randint(20, 30))
