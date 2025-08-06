e9264df662c70c54c47a91491f1ebd9d11ca8a35
inferixon <alekzmay@gmail.com>
2025-08-06 20:29:44 +0200

    Initialize variables for starship, laser, asteroid, and additional game elements in main.ts

--- main.ts
+++ main.ts
@@ -2,3 +2,9 @@ namespace SpriteKind {
     export const Weapon = SpriteKind.create()
     export const Asteroid = SpriteKind.create()
 }
+
+let starship: Sprite = null
+let laser: Sprite = null
+let asteroid: Sprite = null
+let stjerne: Sprite = null
+let ast = 0
